591ec926ff6d0326b391c24fbf2710f6ffde8f8c
michaeljimson <897638991@qq.com>
2022-07-27 22:09:36 +0800

    20220727Push

---
+++ demo.go
@@ -0,0 +1,63 @@
+package main
+
+/* "Golearning/user"
+"fmt" */
+/* func getNameAndAge() (name string, age int) {
+	return "tom", 20
+} */
+func main() {
+
+	/* //匿名变量
+	_, age := getNameAndAge()
+	//fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	*/
+	/* //短变量声明 :=
+	name := "tom"
+	age := 20
+	b := true
+
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	fmt.Printf("b: %v\n", b)
+	*/
+	/* // 批量初始化
+	var name, age, b = "tom", 20, true
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	fmt.Printf("b: %v\n", b)
+	*/
+	/* // 类型推断
+
+	var name = "jimson"
+	var age = 30
+	var b = true
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	fmt.Printf("b: %v\n", b)
+	*/
+	/* var name string = "jimson"
+	var site string = "duoke360.com"
+	var age int = 30 */
+
+	/* var str string
+	var age int
+	var m bool */
+	/* s := user.Hello()
+	fmt.Printf("s: %v\n", s) */
+
+	/* var (
+		name string
+		age  int
+		b    bool
+	)
+
+	name = "tom"
+	age = 20
+	b = true
+
+	fmt.Printf("name: %v\n", name)
+	fmt.Printf("age: %v\n", age)
+	fmt.Printf("b: %v\n", b)
+	*/
+}
---
+++ test_const.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+	/* //常量
+	const PI float32 = 3.14
+	const PI2 = 3.1415
+	fmt.Printf("PI: %v\n", PI)
+	fmt.Printf("PI2: %v\n", PI2)
+
+
+
+	const (
+		a1 = iota // 0
+		a2 = iota // i++
+		a3 = iota
+	)
+	fmt.Printf("a1: %v\n", a1)
+	fmt.Printf("a2: %v\n", a2)
+	fmt.Printf("a3: %v\n", a3)
+
+	*/
+	/*   */
+}
