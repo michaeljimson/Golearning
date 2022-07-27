9aa070711b51ad80c953e3b0a287bdcbe7ee6ddc
michaeljimson <897638991@qq.com>
2022-07-27 22:12:26 +0800

    20220727newPush

---
+++ test_datatype.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+
+	/* a := []int{1, 2, 3}
+	fmt.Printf("%T\n", a ) */ //还可以写函数、结构体、接口等等
+
+	/* a := [2]int{1, 2}
+	fmt.Printf("%T\n", a) */
+
+	/* a := 100
+	p := &a
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", p) */
+	/* var name string = "tom"
+	age := 20
+	b := true
+
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", b) */
+}
